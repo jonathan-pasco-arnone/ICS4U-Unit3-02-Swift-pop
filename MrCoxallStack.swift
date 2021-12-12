34ef3e79b527dbf64b37b24598057f0a46dd1b0f
Ubuntu <ubuntu@ip-172-31-84-55.ec2.internal>
2021-12-12 18:03:06 +0000

    initial commit

--- MrCoxallStack.swift
+++ MrCoxallStack.swift
@@ -1,14 +1,18 @@
+// The whole defining class
 class MrCoxallStack {
     var stackAsArray: [Int] = []
 
+    // pushes a new number into the array
     func push(pushNumber: Int) {
         stackAsArray.insert(pushNumber, at: 0)
     }
 
+    // prints the full stack
     func showStack() {
         print(stackAsArray)
     }
 
+    // removes the top value of the stack
     func pop() {
         stackAsArray.remove(at: 0)
     }
